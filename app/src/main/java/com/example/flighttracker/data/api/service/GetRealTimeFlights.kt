8c5cafe52246bb71c19a8a77fc468aa91c6eaff6
Saydullin <saydullindev@gmail.com>
2023-07-07 16:24:12 +0300

    GetRealTimeFlights service added

--- app/src/main/java/com/example/flighttracker/data/api/service/GetRealTimeFlights.kt
+++ app/src/main/java/com/example/flighttracker/data/api/service/GetRealTimeFlights.kt
@@ -1,6 +1,6 @@
 package com.example.flighttracker.data.api.service
 
-import com.example.flighttracker.data.model.FlightResponse
+import com.example.flighttracker.data.model.flight.FlightResponse
 import retrofit2.Response
 import retrofit2.http.GET
 
