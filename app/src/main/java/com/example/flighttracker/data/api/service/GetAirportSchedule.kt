c96aa9f735c7c571c4c3cf0179d8633ecd091381
Saydullin <saydullindev@gmail.com>
2023-07-07 16:24:29 +0300

    GetAirportSchedule service added

---
+++ app/src/main/java/com/example/flighttracker/data/api/service/GetAirportSchedule.kt
@@ -0,0 +1,12 @@
+package com.example.flighttracker.data.api.service
+
+import com.example.flighttracker.data.model.airport.AirportScheduleResponse
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface GetAirportSchedule {
+
+    @GET
+    suspend fun getAirportSchedule(): Response<AirportScheduleResponse>
+
+}
