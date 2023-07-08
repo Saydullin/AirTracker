95e2f51fdb577e52e1d5b8244ceef415b1005759
Saydullin <saydullindev@gmail.com>
2023-07-09 00:05:45 +0300

    entities & converters to database added

--- app/src/main/java/com/example/flighttracker/data/db/AppDatabase.kt
+++ app/src/main/java/com/example/flighttracker/data/db/AppDatabase.kt
@@ -2,23 +2,44 @@ package com.example.flighttracker.data.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.flighttracker.data.db.converters.ClientRequestConverter
+import com.example.flighttracker.data.db.converters.ConnectionRequestConverter
+import com.example.flighttracker.data.db.converters.GeoRequestConverter
+import com.example.flighttracker.data.db.converters.KarmaRequestConverter
+import com.example.flighttracker.data.db.converters.KeyRequestConverter
 import com.example.flighttracker.data.db.dao.FlightDao
-import com.example.flighttracker.data.db.entities.ClientRequestEntity
-import com.example.flighttracker.data.db.entities.FlightEntity
-import com.example.flighttracker.data.db.entities.FlightsListEntity
-import com.example.flighttracker.data.db.entities.ParamsRequestEntity
-import com.example.flighttracker.data.db.entities.RequestEntity
+import com.example.flighttracker.data.db.entities.airport.AirportEntity
+import com.example.flighttracker.data.db.entities.request.ClientRequestEntity
+import com.example.flighttracker.data.db.entities.flight.FlightEntity
+import com.example.flighttracker.data.db.entities.request.ConnectionRequestEntity
+import com.example.flighttracker.data.db.entities.request.GeoRequestEntity
+import com.example.flighttracker.data.db.entities.request.KarmaRequestEntity
+import com.example.flighttracker.data.db.entities.request.KeyRequestEntity
+import com.example.flighttracker.data.db.entities.request.ParamsRequestEntity
+import com.example.flighttracker.data.db.entities.request.RequestEntity
 
 @Database(
     entities = [
+        AirportEntity::class,
         ClientRequestEntity::class,
         FlightEntity::class,
-        FlightsListEntity::class,
         ParamsRequestEntity::class,
         RequestEntity::class,
+        GeoRequestEntity::class,
+        ConnectionRequestEntity::class,
+        KarmaRequestEntity::class,
+        KeyRequestEntity::class,
     ],
     version = 1,
     exportSchema = true,
+)
+@TypeConverters(
+    GeoRequestConverter::class,
+    ConnectionRequestConverter::class,
+    ClientRequestConverter::class,
+    KarmaRequestConverter::class,
+    KeyRequestConverter::class,
 )
 abstract class AppDatabase : RoomDatabase() {
 
