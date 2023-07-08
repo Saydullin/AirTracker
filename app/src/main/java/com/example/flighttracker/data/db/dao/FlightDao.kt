99a33f923df0d8537e9bf63f5faf0ab6f5f8606b
Saydullin <saydullindev@gmail.com>
2023-07-09 00:04:47 +0300

    FlightDao.kt added

--- app/src/main/java/com/example/flighttracker/data/db/dao/FlightDao.kt
+++ app/src/main/java/com/example/flighttracker/data/db/dao/FlightDao.kt
@@ -2,12 +2,12 @@ package com.example.flighttracker.data.db.dao
 
 import androidx.room.Dao
 import androidx.room.Query
-import com.example.flighttracker.data.db.entities.FlightsListEntity
+import com.example.flighttracker.data.db.entities.flight.FlightEntity
 
 @Dao
 interface FlightDao {
 
-    @Query("SELECT * FROM FlightsListEntity")
-    fun getAll(): List<FlightsListEntity>
+    @Query("SELECT * FROM FlightEntity")
+    fun getAll(): List<FlightEntity>
 
 }
