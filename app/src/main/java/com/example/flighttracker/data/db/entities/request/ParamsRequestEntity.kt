4120e3b43a3042a4d8a7b5119eb5a0c125c9ffce
Saydullin <saydullindev@gmail.com>
2023-07-09 00:01:55 +0300

    ParamsRequestEntity.kt added

---
+++ app/src/main/java/com/example/flighttracker/data/db/entities/request/ParamsRequestEntity.kt
@@ -0,0 +1,12 @@
+package com.example.flighttracker.data.db.entities.request
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class ParamsRequestEntity(
+    @PrimaryKey val id: Int,
+    val lang: String,
+)
+
+
